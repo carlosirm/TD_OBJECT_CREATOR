51903456670afa4ffae58ee3f95b9dcc38c8ef50
Carlos I. Rosas M <cirosasm@gmail.com>
2022-04-29 17:23:12 -0300

    ignorar .pyc

--- objetos_td.bat
+++ objetos_td.bat
@@ -1,3 +1,6 @@
-cd: C:\Users\CR250036\Desktop\TD_OBJECT_CREATOR
+cd: C:\Users\CarlosRosasM\My Drive (carlos.rosasmonasterios@gmail.com)\REPOS_GIT\TD_OBJECT_CREATOR
 python t002_object_creator.py
 cmd /k
+
+
+
