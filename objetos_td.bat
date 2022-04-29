ea7dab27e8929a02b49e3d510413a5e25b0dae15
Carlos I. Rosas M <cirosasm@gmail.com>
2022-04-29 20:04:13 -0300

    Version Estable Inicial

--- objetos_td.bat
+++ objetos_td.bat
@@ -1,4 +1,4 @@
-cd: C:\Users\CarlosRosasM\My Drive (carlos.rosasmonasterios@gmail.com)\REPOS_GIT\TD_OBJECT_CREATOR
+cd: C:\Users\CarlosRosasM\Documents\REPOS_GIT\TD_OBJECT_CREATOR
 python t002_object_creator.py
 cmd /k
 
